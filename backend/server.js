9a032421ca6c91bc5aabb75a7ab69547e78d472f
Izgardon <williamsessions23@gmail.com>
2022-10-08 19:26:17 +0100

    created server and routes

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+app.use(cors());
+
+//Routes
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Welcome to BlueSquare database"));
+
+const authRoutes = require("./routes/auth");
+app.use("/auth", authRoutes);
+const employeeDataRoutes = require("./routes/employeeData");
+app.use("/employeeData", employeeDataRoutes);
+
+app.listen(process.env.PORT || 8080, () => {
+  const PORT = process.env.PORT || 8080;
+  console.log(`server running  in ${process.env.NODE_ENV} mode on ${PORT}`);
+});
+
+module.exports = app;
